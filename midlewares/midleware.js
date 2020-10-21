081aa13375ecfc945dbad05449f5c32672c73f29
mikelovex <maicon.rafael17@gmail.com>
2020-10-20 21:59:04 -0300

    midleware de autenticação

---
+++ midlewares/midleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+// criando segredo para usar no token
+const jwtSecret = 'dsaduahxjkbnzbcmguiebhxgywgiorqacxzm,bfbnmvhsgdsa'
+
+
+function auth(req, res, next) {
+    const authToken = req.headers['authorization']
+
+    if(authToken != undefined){
+
+        const bearer = authToken.split(' ')
+        const token = bearer[1]
+
+        jwt.verify(token, jwtSecret, (err, data) => {
+            if(err) {
+                res.status(401).json('token invalido')
+            }else{
+                next()
+            }
+        })
+    }else {
+        res.status(401).json({err: 'token invalido!'})
+    }
+}
+
+
+module.exports = auth
